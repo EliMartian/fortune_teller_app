7f7d754b44e6c4fc1ba1c716b32998e199d8e709
emartin <eamart34@uw.edu>
2024-04-27 22:26:43 -0700

    semi working upload and share portfolio feature

---
+++ Fortune Teller/DataParsingService.swift
@@ -0,0 +1,36 @@
+//
+//  DataParsingService.swift
+//  Fortune Teller
+//
+//  Created by E Martin on 4/27/24.
+//
+
+import Foundation
+import Combine
+
+class DataParsingService: ObservableObject {
+    @Published var parsedPortfolio: Portfolio?
+    @Published var parsingError: Error?
+    
+    func parseSharedPortfolio(from text: String) {
+        guard let data = text.data(using: .utf8) else {
+            self.parsingError = NSError(domain: "DataParsingService", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid data"])
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let sharedData = try decoder.decode(SharedPortfolioData.self, from: data)
+            let parsedPortfolio = Portfolio(name: sharedData.name, tickerSymbols: sharedData.tickerSymbols)
+            self.parsedPortfolio = parsedPortfolio
+        } catch {
+            self.parsingError = error
+        }
+    }
+}
+
+// Struct representing shared portfolio data format
+struct SharedPortfolioData: Codable {
+    let name: String
+    let tickerSymbols: [String]
+}
--- Fortune Teller/PortfolioView/PortfolioDetailedView.swift
+++ Fortune Teller/PortfolioView/PortfolioDetailedView.swift
@@ -9,19 +9,24 @@ import Foundation
 import SwiftUI
 
 struct PortfolioDetailedView: View {
+    @EnvironmentObject private var dataParsingService: DataParsingService
     let portfolio: Portfolio
     let onDelete: () -> Void
+    
+    // Binding to control the presentation of the alert
+    @State private var isShowingAlert = false
+    @State private var sharedText: SharedTextItem? = nil
 
     var body: some View {
         VStack {
             Spacer()
             Text("Portfolio Name: \(portfolio.name)")
                 .padding()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.blue)
 
             Text("Ticker Symbols:")
                 .padding(.top, 10)
-                .foregroundColor(.red)
+                .foregroundColor(.blue)
 
             List(portfolio.tickerSymbols, id: \.self) { tickerSymbol in
                 Text(tickerSymbol)
@@ -34,7 +39,45 @@ struct PortfolioDetailedView: View {
             }
             .foregroundColor(.red)
             .padding()
+            
+            Button(action: {
+                // Generate JSON representation of the portfolio
+                if let jsonData = try? JSONEncoder().encode(portfolio),
+                   let jsonString = String(data: jsonData, encoding: .utf8) {
+                    sharedText = SharedTextItem(text: jsonString)
+                } else {
+                    sharedText = nil
+                    isShowingAlert = true
+                }
+            }) {
+                Image(systemName: "square.and.arrow.up")
+                    .foregroundColor(.blue)
+            }
+            .padding() // Add padding to the share button
         }
         .navigationTitle("Portfolio Details")
+        .onAppear {
+            // Show the alert if there's an error in generating the JSON string
+            if dataParsingService.parsingError != nil {
+                isShowingAlert = true
+            }
+        }
+        .alert(isPresented: $isShowingAlert) {
+            Alert(
+                title: Text("Error"),
+                message: Text("Failed to generate JSON representation"),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .sheet(item: $sharedText) { sharedTextItem in
+            // Present custom ActivityViewController to share the JSON string
+            ActivityViewController(activityItems: [sharedTextItem.text])
+        }
     }
+}
+
+
+struct SharedTextItem: Identifiable {
+    let id = UUID()
+    let text: String
 }
