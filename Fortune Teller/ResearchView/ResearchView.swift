cbb7847437b5c702502769302ee53e9a0d670add
emartin <eamart34@uw.edu>
2024-05-12 22:30:52 -0700

    adding in slider feature for scalable investment size

--- Fortune Teller/ResearchView/ResearchView.swift
+++ Fortune Teller/ResearchView/ResearchView.swift
@@ -14,14 +14,14 @@ struct ResearchView: View {
     @State private var yieldCalculations: [YieldCalculation] = []
     @State private var stockData: [StockDataPoint] = []
     @State private var errorMessage: String = ""
-    @State private var cancellables = Set<AnyCancellable>() // Declare cancellables as @State property
-    
-    // Swift working???
-    
+    @State private var investmentAmount: Double = 10000.0 // Initial investment amount
+    @State private var showSlider: Bool = false
+    @State private var cancellables = Set<AnyCancellable>()
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
-                VStack() {
+                VStack {
                     ZStack {
                         Color.black.edgesIgnoringSafeArea(.all)
                         
@@ -32,65 +32,67 @@ struct ResearchView: View {
                             
                             Button("Search") {
                                 if !ticker.isEmpty {
-                                    //                    print("The stock the user put was: \(ticker)")
                                     doResearch()
+                                    showSlider = true // Show slider after search
                                 }
                             }
                             Spacer()
                         }
                         .padding()
                         
-                        Spacer()
-                        
                         if !stockData.isEmpty {
                             SecurityGraphView(stockData: stockData, ticker: ticker)
                                 .frame(height: self.calculateGraphHeight(geometry: geometry))
-                                .frame(width: self.calculateGraphHeight(geometry: geometry))
                                 .padding(.top, self.calculateVerticalPaddingTop(geometry: geometry))
                                 .padding(.bottom, self.calculateVerticalPaddingBottom(geometry: geometry))
                                 .clipped()
-                            
                         }
                         
-                        Spacer()
-                        
-                        if !errorMessage.isEmpty {
-                            Text(errorMessage)
-                                .foregroundColor(.red)
-                                .padding()
-                        }
-                        
-                    }
-                    VStack {
-                        // Display yield calculations
-                        ForEach(yieldCalculations) { calculation in
-                            if let yield = calculation.yield {
-                                YieldView(calculation: calculation)
+                        if showSlider {
+                            VStack {
+                                Text("Investment Amount: $\(Int(investmentAmount))")
+                                    .foregroundColor(.white)
+                                    .padding(.bottom, 10) // Add bottom padding to separate from the Slider
+                                
+                                Slider(value: $investmentAmount, in: 1000...100000, step: 1000)
+                                    .padding(.horizontal)
+                                    .accentColor(.green)
                             }
+                            .padding(.top, self.calculatePercentageHeight(geometry: geometry, percentage: 0.65)) // Adjust the top padding
+                        }
+                    }
+                    
+                    // Display yield calculations
+                    ForEach(yieldCalculations) { calculation in
+                        if let yield = calculation.yield {
+                            YieldView(calculation: calculation, investmentAmount: investmentAmount)
                         }
                     }
                     .background(Color.white)
                     .cornerRadius(5)
-                    Spacer()
                     
+                    Spacer() // Add Spacer to push content to the top
                 }
-//                .frame(width: geometry.size.width, height: geometry.size.height * 0.90)
             }
             .frame(width: geometry.size.width, height: geometry.size.height * 0.90)
         }
-        .background(Color.black.edgesIgnoringSafeArea(.all)) // Apply background color to outer VStack
+        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
     
+    private func calculatePercentageHeight(geometry: GeometryProxy, percentage: CGFloat) -> CGFloat {
+        return geometry.size.height * percentage
+    }
+
     private func calculateGraphHeight(geometry: GeometryProxy) -> CGFloat {
-        return geometry.size.height * 0.45 // Adjust as needed (e.g., 40% of screen height for graph)
+        return geometry.size.height * 0.45
     }
     
     private func calculateVerticalPaddingTop(geometry: GeometryProxy) -> CGFloat {
-        return geometry.size.height * 0.20 // Adjust as needed (e.g., 10% of screen height for vertical padding)
+        return geometry.size.height * 0.20
     }
     
     private func calculateVerticalPaddingBottom(geometry: GeometryProxy) -> CGFloat {
-        return geometry.size.height * 0.10 // Adjust as needed (e.g., 10% of screen height for vertical padding)
+        return geometry.size.height * 0.10
     }
     
     private func doResearch() {
@@ -106,13 +108,11 @@ struct ResearchView: View {
         URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .tryMap { data -> [StockDataPoint] in
-                // Attempt to parse JSON data dynamically
                 guard let res = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
                       let timeSeries = res["Weekly Adjusted Time Series"] as? [String: [String: String]] else {
                     throw NSError(domain: "Invalid JSON", code: 0, userInfo: nil)
                 }
                 
-                // Convert API response to array of StockDataPoint objects
                 var dataPoints: [StockDataPoint] = []
                 for (dateString, priceData) in timeSeries {
                     if let adjustedClose = Double(priceData["5. adjusted close"] ?? "0.0"),
@@ -120,7 +120,6 @@ struct ResearchView: View {
                         dataPoints.append(StockDataPoint(date: date, adjustedClose: adjustedClose))
                     }
                 }
-                // Sort dataPoints array by date
                 dataPoints.sort { $0.date < $1.date }
                 
                 return dataPoints
@@ -131,10 +130,8 @@ struct ResearchView: View {
                     errorMessage = "Error: \(error.localizedDescription)"
                 }
             }, receiveValue: { response in
-                // Handle successful response
                 stockData = response
                 
-                // Calculate yield over 10 years
                 let yearsToCalculate: [Int] = [10, 5, 2, 1]
                 yieldCalculations = yearsToCalculate.map { years in
                     if let yield = calculateNYearsYield(yearsAgo: years, dataPoints: stockData) {
@@ -144,32 +141,27 @@ struct ResearchView: View {
                     }
                 }
                 
-                // Calculate YTD yield
                 if let ytdYield = calculateYTDYield(dataPoints: stockData) {
-                    yieldCalculations.append(YieldCalculation(years: 0, yield: ytdYield)) // 0 years for YTD
+                    yieldCalculations.append(YieldCalculation(years: 0, yield: ytdYield))
                 }
             })
-            .store(in: &cancellables) // Store cancellable in @State property
+            .store(in: &cancellables)
     }
     
-    // Function to calculate yield over N years ago
     private func calculateNYearsYield(yearsAgo: Int, dataPoints: [StockDataPoint]) -> Double? {
         guard !dataPoints.isEmpty else { return nil }
 
-        // Calculate date N years ago
         let calendar = Calendar.current
         let currentDate = Date()
         let startDate = calendar.date(byAdding: .year, value: -yearsAgo, to: currentDate)!
 
-        // Find the closest data point to the start date
         guard let closestDataPoint = dataPoints.min(by: { abs($0.date.timeIntervalSince(startDate)) < abs($1.date.timeIntervalSince(startDate)) }) else {
             return nil
         }
 
-        // Calculate yield over N years
-        let initialInvestment = 10000.0
+        let initialInvestment = investmentAmount
         let initialClose = closestDataPoint.adjustedClose
-        let finalClose = dataPoints.last?.adjustedClose ?? 0.0 // Use last data point's close as final close
+        let finalClose = dataPoints.last?.adjustedClose ?? 0.0
 
         let yield = (initialInvestment / initialClose) * finalClose
         return yield
@@ -178,74 +170,66 @@ struct ResearchView: View {
     private func calculateYTDYield(dataPoints: [StockDataPoint]) -> Double? {
         guard !dataPoints.isEmpty else { return nil }
 
-        // Determine the start date of the current year
         let calendar = Calendar.current
         let currentDate = Date()
         let startOfYear = calendar.startOfDay(for: calendar.date(from: calendar.dateComponents([.year], from: currentDate))!)
 
-        // Find the closest data point to the start of the year
         guard let closestDataPoint = dataPoints.min(by: { abs($0.date.timeIntervalSince(startOfYear)) < abs($1.date.timeIntervalSince(startOfYear)) }) else {
             return nil
         }
 
-        // Calculate yield from start of the year to the most recent data point
-        let initialInvestment = 10000.0
+        let initialInvestment = investmentAmount
         let initialClose = closestDataPoint.adjustedClose
-        let finalClose = dataPoints.last?.adjustedClose ?? 0.0 // Use last data point's close as final close
+        let finalClose = dataPoints.last?.adjustedClose ?? 0.0
 
         let yield = (initialInvestment / initialClose) * finalClose
         return yield
     }
-
 }
 
-
 struct YieldCalculation: Identifiable {
-    let id = UUID() // Unique identifier for each instance
+    let id = UUID()
     let years: Int
     let yield: Double?
 }
 
 struct YieldView: View {
     let calculation: YieldCalculation
+    let investmentAmount: Double
 
     var body: some View {
         if let yield = calculation.yield {
             if calculation.years == 0 {
-                // Display YTD yield
-                return Text("Calculated Yield on YTD: $\(yield, specifier: "%.2f")")
+                return Text("Calculated Yield on YTD: $\(calculateYield(investmentAmount: investmentAmount, yield: yield), specifier: "%.2f")")
                     .foregroundColor(.black)
                     .padding()
             } else {
-                // Display yield for specified years
-                return Text("Calculated Yield on \(calculation.years) Years: $\(yield, specifier: "%.2f")")
+                return Text("Calculated Yield on \(calculation.years) Years: $\(calculateYield(investmentAmount: investmentAmount, yield: yield), specifier: "%.2f")")
                     .foregroundColor(.black)
                     .padding()
             }
         } else {
-            // Display failure message for the specified years
             return Text("Failed to calculate yield for \(calculation.years == 0 ? "YTD" : "\(calculation.years) Years")")
                 .foregroundColor(.red)
                 .padding()
         }
     }
-}
     
+    private func calculateYield(investmentAmount: Double, yield: Double) -> Double {
+        return yield * (investmentAmount / 10000.0) // Assuming 10000.0 is the initial investment for yield calculation
+    }
+}
 
-// Custom StockDataPoint struct representing a data point in the stock time series
 struct StockDataPoint {
     var date: Date
     var adjustedClose: Double
 }
 
-// Extension to parse ISO8601 date format
 extension DateFormatter {
     static let iso8601: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-//        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         return formatter
     }()
 }
-
